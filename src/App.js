018e75d0fa1999f218121ba3bbee4de157e5ddc1
Audywb <woranat.bo.62@ubu.ac.th>
2021-10-11 22:41:56 +0700

    Quiz room

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {createContext, useState} from "react"
+import Menu from "./components/Menu"
+import Quiz from "./components/Quiz"
+import Score from "./components/Score"
+
+export const DataContext = createContext()
 
 function App() {
+  const [appState,setAppState] = useState("menu")
+  const [score,setScore] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
+      <div className="App">
+      <h1>แบบทดสอบ</h1>
+      {appState === "menu" && <Menu/>}
+      {appState === "quiz" && <Quiz/>}
+      {appState === "score" && <Score/>}
     </div>
+    </DataContext.Provider>
   );
 }
 
